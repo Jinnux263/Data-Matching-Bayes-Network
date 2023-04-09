c3475c90c322477daee66e106f1c9931a57040d9
Jinnux <nguyentandat26032001@gmail.com>
2023-04-09 10:34:08 +0700

    refactor: 💡 add some test config

--- main.py
+++ main.py
@@ -42,25 +42,25 @@ def setup_Network():
 
   # Set up the graph
   arc_Match_Address = Arc(Match,Address)
-  # arc_Match_Author = Arc(Match,Author)
-  # arc_Match_Page = Arc(Match,Page)
-  # arc_Match_Publisher = Arc(Match,Publisher)
-  # arc_Match_Title = Arc(Match,Title)
-  # arc_Match_Venue = Arc(Match,Venue)
-  # arc_Match_Year = Arc(Match,Year)
+  arc_Match_Author = Arc(Match,Author)
+  arc_Match_Page = Arc(Match,Page)
+  arc_Match_Publisher = Arc(Match,Publisher)
+  arc_Match_Title = Arc(Match,Title)
+  arc_Match_Venue = Arc(Match,Venue)
+  arc_Match_Year = Arc(Match,Year)
 
   # Check table size
   # print (Address.getTableSize())
 
   # Conditional distribution for node 'Match'
-  Match.setProbabilities([0.02,0.98])
-  Address.setProbabilities([0.96, 0.04, 0, 0.01, 0.3, 0.69])
-  Author.setProbabilities([0.96, 0.04, 0])
-  Page.setProbabilities([0.96, 0.04, 0])
-  Publisher.setProbabilities([0.96, 0.04, 0])
-  Title.setProbabilities([0.96, 0.04, 0])
-  Venue.setProbabilities([0.96, 0.04, 0])
-  Year.setProbabilities([0.96, 0.04, 0])
+  Match.setProbabilities([0.0101526723,1 - 0.0101526723])
+  Address.setProbabilities([0.5, 0.3, 0.2, 0.05, 0.3, 0.65])
+  Author.setProbabilities([0.5, 0.3, 0.2, 0.05, 0.3, 0.65])
+  Page.setProbabilities([0.5, 0.3, 0.2, 0.05, 0.3, 0.65])
+  Publisher.setProbabilities([0.5, 0.3, 0.2, 0.05, 0.3, 0.65])
+  Title.setProbabilities([0.5, 0.3, 0.2, 0.05, 0.3, 0.65])
+  Venue.setProbabilities([0.5, 0.3, 0.2, 0.05, 0.3, 0.65])
+  Year.setProbabilities([0.1, 0.4, 0.5, 0.03, 0.42, 0.55])
 
   # net.addNodes([Address, Match])
   net.addNodes([Match, Address, Author, Page, Publisher, Title, Venue, Year])
@@ -69,14 +69,17 @@ def setup_Network():
 
 
 def calculate_match(net, Address, Author, Page, Publisher, Title, Venue, Year):
+  
+  # For testing
+  # net.setEvidence('Title', 1)
   # Set evidence
   net.setEvidence('Address', Address)
-  # net.setEvidence('Author', Address)
-  # net.setEvidence('Page', Page)
-  # net.setEvidence('Publisher', Publisher)
-  # net.setEvidence('Title', Title)
-  # net.setEvidence('Venue', Venue)
-  # net.setEvidence('Year', Year)
+  net.setEvidence('Author', Author)
+  net.setEvidence('Page', Page)
+  net.setEvidence('Publisher', Publisher)
+  net.setEvidence('Title', Title)
+  net.setEvidence('Venue', Venue)
+  net.setEvidence('Year', Year)
 
   net.computeBeliefs()
 
@@ -84,7 +87,26 @@ def calculate_match(net, Address, Author, Page, Publisher, Title, Venue, Year):
 # Define a main() function.
 def main():
   net, Match, Address, Author, Page, Publisher, Title, Venue, Year = setup_Network()
-  calculate_match(net, Address = 1, Author = 1, Page = 1, Publisher = 1, Title = 1, Venue = 1, Year = 1)
+
+
+  # Setup Evident: 0 - match, 1 - unknown, 2 - notmatch
+  AddressEvident = 2
+  AuthorEvident = 1
+  PageEvident = 2
+  PublisherEvident = 2
+  TitleEvident = 1
+  VenueEvident = 3
+  YearEvident = 1
+
+
+  calculate_match(net,
+  Address = AddressEvident,
+  Author = AuthorEvident,
+  Page = PageEvident,
+  Publisher = PublisherEvident,
+  Title = TitleEvident,
+  Venue =   VenueEvident,
+  Year = YearEvident)
 
   # Print the results for each node
   print("RESULT:")
